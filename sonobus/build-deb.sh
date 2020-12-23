00a6a392cecb1b787da18c557207a41f3fb1824f
kdoren <kevin@doren.org>
2020-12-22 18:46:05 -0800

    add sonobus package

---
+++ sonobus/build-deb.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cd $DIR
+
+PACKAGE=sonobus
+VERSION=1.2.0
+TAG=1.2.0
+
+PKGFOLDER=${PACKAGE}-${VERSION}
+
+cd src
+git checkout tags/${TAG}
+cd ..
+
+mkdir -p ${PKGFOLDER}
+cp -r src/* ${PKGFOLDER}
+tar -czf ${PKGFOLDER}.tar.gz --exclude .git ${PKGFOLDER}
+
+cd ${PKGFOLDER}
+
+echo y | debuild -b -us -uc -j`nproc`
