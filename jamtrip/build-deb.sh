0a35f5f4594604d772a60252c54dc6ea7ff4a4f9
kdoren <kevin@doren.org>
2021-01-02 13:56:32 -0800

    add jamtrip package

---
+++ jamtrip/build-deb.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#
+# to build on Raspberry Pi requires:
+# 
+# # install nodejs
+# curl -sL https://deb.nodesource.com/setup_14.x -o nodesource_setup.sh
+# sudo bash nodesource_setup.sh
+# sudo apt install nodejs
+#
+# # install yarn
+# curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+# echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+# sudo apt update && sudo apt install yarn
+#
+# # install fpm
+# sudo apt-get install ruby-full
+# sudo gem install fpm
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cd $DIR
+
+PACKAGE=jamtrip
+VERSION=0.1.2
+TAG=v0.1.2
+
+PKGFOLDER=${PACKAGE}-${VERSION}
+
+cd src
+git checkout tags/${TAG}
+cd ..
+
+mkdir -p ${PKGFOLDER}
+cp -r src/* ${PKGFOLDER}
+#tar -czf ${PKGFOLDER}.tar.gz --exclude .git ${PKGFOLDER}
+
+cd ${PKGFOLDER}
+/bin/cp -f ../package.json .
+export NODE_OPTIONS=--experimental-worker
+export USE_SYSTEM_FPM=true
+yarn
+yarn package-linux
+
