2dbe1f351bc60dc2d7995f3586f78abb3f319adf
kdoren <kevin@doren.org>
2021-04-04 07:37:14 -0700

    update sonobus to version 1.4.3

--- sonobus/build-deb.sh
+++ sonobus/build-deb.sh
@@ -5,8 +5,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR
 
 PACKAGE=sonobus
-VERSION=1.4.2
-TAG=1.4.2
+VERSION=1.4.3
+TAG=1.4.3
 
 PKGFOLDER=${PACKAGE}-${VERSION}
 
