6d266cc8ff4f90f6f0a10d2c3d309ea54b463e8f
kdoren <kevin@doren.org>
2021-04-18 10:24:48 -0700

    update sonobus to 1.4.5

--- sonobus/build-deb.sh
+++ sonobus/build-deb.sh
@@ -5,14 +5,19 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR
 
 PACKAGE=sonobus
-VERSION=1.4.3
-TAG=1.4.3
+VERSION=1.4.5
+TAG=1.4.5
 
 PKGFOLDER=${PACKAGE}-${VERSION}
 
 cd src
 git checkout tags/${TAG}
 cd ..
+
+if [ -d ${PKGFOLDER} ]; then
+  rm -rf ${PKGFOLDER}/{build,doc,localization,scripts,images,mobile,Source,JuceLibraryCode,obj-arm-linux-gnueabihf,deps,linux,release,snap}
+  rm -f ${PKGFOLDER}/*.sh ${PKGFOLDER}/{SonoBus.jucer,CMakeLists.txt,LICENSE,README.md}
+fi
 
 mkdir -p ${PKGFOLDER}
 cp -r src/* ${PKGFOLDER}
