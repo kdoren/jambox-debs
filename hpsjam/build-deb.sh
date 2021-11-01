85874bde280b21abf69394820fbeadc512501b31
kdoren <kevin@doren.org>
2021-11-01 10:30:03 -0700

    update hpsjam to v1.0.17

--- hpsjam/build-deb.sh
+++ hpsjam/build-deb.sh
@@ -5,8 +5,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR
 
 PACKAGE=hpsjam
-VERSION=1.0.16
-TAG=v1.0.16
+VERSION=1.0.17
+TAG=v1.0.17
 
 PKGFOLDER=${PACKAGE}-${VERSION}
 
