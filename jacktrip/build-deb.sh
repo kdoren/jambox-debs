8e768f50f790638cab7eb22d668a7d2aac1e4738
kdoren <kevin@doren.org>
2021-11-04 00:20:44 -0700

    update jacktrip to 1.4.0

--- jacktrip/build-deb.sh
+++ jacktrip/build-deb.sh
@@ -5,18 +5,26 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR
 
 PACKAGE=jacktrip
-VERSION=1.3.0
-TAG=v1.3.0
+VERSION=1.4.0
+TAG=v1.4.0
 
 PKGFOLDER=${PACKAGE}-${VERSION}
 
 cd src
+git checkout main
+git submodule update --init
+git pull --recurse-submodules
 git checkout tags/${TAG}
 cd ..
 
+if [ -d ${PKGFOLDER} ]; then
+  rm -rf ${PKGFOLDER}/{builddir,cross,docs,documentation,externals,faust-src,LICENSES,linux,macos,papers,scripts,src,subprojects,tests,win}
+  rm -f ${PKGFOLDER}/*.txt ${PKGFOLDER}/*.pro ${PKGFOLDER}/LICENSE* ${PKGFOLDER}/meson* ${PKGFOLDER}/INSTALL* ${PKGFOLDER}/{Doxyfile,jacktrip+doxygen,meson.build,mkdocs.yml}
+fi
+
 mkdir -p ${PKGFOLDER}
 cp -r src/* ${PKGFOLDER}
-tar -czf ${PKGFOLDER}.tar.gz --exclude .git ${PKGFOLDER}
+# tar -czf ${PKGFOLDER}.tar.gz --exclude .git ${PKGFOLDER}
 
 cd ${PKGFOLDER}
 
