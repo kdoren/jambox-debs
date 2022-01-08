2568fa203d9967749aa4001d8be0bf85223bbb0d
kdoren <kevin@doren.org>
2022-01-08 01:35:22 -0800

    update jamtaba build process

--- jamtaba/build-deb.sh
+++ jamtaba/build-deb.sh
@@ -8,30 +8,33 @@ PACKAGE=jamtaba
 VERSION=2.1.16
 TAG=v2.1.16
 
-VST3SDK_TAG=v3.7.1_build_50
+#VST3SDK_TAG=v3.7.1_build_50
+VST3SDK_TAG=v3.7.4_build_25
 
 PKGFOLDER=${PACKAGE}-${VERSION}
 
 cd src
+git checkout master
+git pull
 git checkout tags/${TAG}
 cd ..
 
 cd vst3sdk
+git checkout master
+git submodule update --init
+git pull --recurse-submodules
 git checkout tags/${VST3SDK_TAG}
 cd ..
 
-cp -r VST_SDK_2.4/* vst3sdk/
-
-mkdir -p ${PKGFOLDER}
+[[ -d ${PKGFOLDER} ]] && rm -rf ${PKGFOLDER}
+mkdir -p ${PKGFOLDER}/debian
+cp -r debian/* ${PKGFOLDER}/debian/
 cp -r src/* ${PKGFOLDER}
-tar -czf ${PKGFOLDER}.tar.gz --exclude .git ${PKGFOLDER}
 
 cd ${PKGFOLDER}
 mkdir -p VST_SDK
-cd VST_SDK
-ln -sfn ../../vst3sdk VST3_SDK
-ln -sfn ../../VST_SDK_2.4 VST2_SDK
-cd ..
-ln -sfn ../minimp3 minimp3
+cp -pr ../vst3sdk VST_SDK/VST3_SDK
+cp -pr ../VST_SDK_2.4 VST_SDK/VST2_SDK
+cp -pr ../minimp3 minimp3
 
 echo y | debuild -b -us -uc -j`nproc`
