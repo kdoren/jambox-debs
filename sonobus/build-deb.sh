896396a9548de6bc19da1f96c02ff61553efb7ff
kdoren <kevin@doren.org>
2022-01-28 19:35:22 -0800

    update sonobus to 1.4.9

--- sonobus/build-deb.sh
+++ sonobus/build-deb.sh
@@ -5,8 +5,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR
 
 PACKAGE=sonobus
-VERSION=1.4.6
-TAG=1.4.6
+VERSION=1.4.9
+TAG=1.4.9
 
 PKGFOLDER=${PACKAGE}-${VERSION}
 
