bb0d05e269ffe124704c8fe170f16aa37f7846b4
kdoren <kevin@doren.org>
2022-01-29 23:29:50 -0800

    simplify build script

--- midipp/build-deb.sh
+++ midipp/build-deb.sh
@@ -12,20 +12,15 @@ PKGFOLDER=${PACKAGE}-${VERSION}
 
 cd src
 git checkout main
-git pull
-git checkout ${TAG}
 git submodule update --init
+git pull --recurse-submodules
+git checkout tags/${TAG}
 cd ..
 
-if [ -d ${PKGFOLDER} ]; then
-  rm -rf ${PKGFOLDER}/{build,MidiPlayerPro.iconset,android,icons,libumidi,src}
-  rm -f ${PKGFOLDER}/midipp* ${PKGFOLDER}/{LICENSE,README.md}
-fi
-
-mkdir -p ${PKGFOLDER}
+[[ -d ${PKGFOLDER} ]] && rm -rf ${PKGFOLDER}
+mkdir -p ${PKGFOLDER}/debian
+cp -r debian/* ${PKGFOLDER}/debian/
 cp -r src/* ${PKGFOLDER}
-#echo -e "SHLIB_SHFLAGS=-Wl,--soname=lib\${LIB}.so.\${SHLIB_SOVERSION}" >> ${PKGFOLDER}/libumidi/Makefile
-#tar -czf ${PKGFOLDER}.tar.gz --exclude .git ${PKGFOLDER}
 
 cd ${PKGFOLDER}
 
