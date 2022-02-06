5ba82df974690ed1c6cbddbc687b8571bd0aeb61
kdoren <kevin@doren.org>
2022-02-06 20:20:24 +0000

    simplify build script

--- aj-snapshot/build-deb.sh
+++ aj-snapshot/build-deb.sh
@@ -11,12 +11,17 @@ TAG=release_0.9.9
 PKGFOLDER=${PACKAGE}-${VERSION}
 
 cd src
+git checkout master
+#git submodule update --init
+#git pull --recurse-submodules
+git pull
 git checkout tags/${TAG}
 cd ..
 
-mkdir -p ${PKGFOLDER}
+[[ -d ${PKGFOLDER} ]] && rm -rf ${PKGFOLDER}
+mkdir -p ${PKGFOLDER}/debian
+cp -r debian/* ${PKGFOLDER}/debian/
 cp -r src/* ${PKGFOLDER}
-tar -czf ${PKGFOLDER}.tar.gz --exclude .git ${PKGFOLDER}
 
 cd ${PKGFOLDER}
 
