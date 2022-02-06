d30d67dd901b0942d30bc74a8f07b61fcc7594e8
kdoren <kevin@doren.org>
2022-02-06 07:15:45 +0000

    simplify build script

--- alsa-topology-conf/build-deb.sh
+++ alsa-topology-conf/build-deb.sh
@@ -12,18 +12,15 @@ PKGFOLDER=${PACKAGE}-${VERSION}
 
 cd src
 git checkout master
-git pull
-git checkout ${TAG}
+git submodule update --init
+git pull --recurse-submodules
+git checkout tags/${TAG}
 cd ..
 
-if [ -d ${PKGFOLDER} ]; then
-  rm -rf ${PKGFOLDER}/debian/{alsa-topology-conf}
-  rm -f ${PKGFOLDER}/debian/{debhelper-build-stamp,files} ${PKGFOLDER}/debian/autoreconf.* ${PKGFOLDER}/debian/*.debhelper ${PKGFOLDER}/debian/*.substvars ${PKGFOLDER}/debian/*.log
-fi
-
-mkdir -p ${PKGFOLDER}
+[[ -d ${PKGFOLDER} ]] && rm -rf ${PKGFOLDER}
+mkdir -p ${PKGFOLDER}/debian
+cp -r debian/* ${PKGFOLDER}/debian/
 cp -r src/* ${PKGFOLDER}
-#tar -czf ${PKGFOLDER}.tar.gz --exclude .git ${PKGFOLDER}
 
 cd ${PKGFOLDER}
 rm -f Makefile
